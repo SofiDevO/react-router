d03660ea3bcb5626e6d06ee8a5735ec957c30b90
SofiDevO <angiemeow89@gmail.com>
2023-09-03 04:13:55 -0600

    se actualiza api local a vercel api

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
+/** @format */
+
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "http://localhost:5555",
+	baseURL: "https://react-router-api-jet.vercel.app/",
 });
 
-
-export const buscar = async (url, setData) =>{
-    const respuesta = await api.get(url);
-    setData(respuesta.data)
-    
+export const buscar = async (url, setData) => {
+	const respuesta = await api.get(url);
+	setData(respuesta.data);
 };
